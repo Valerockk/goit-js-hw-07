5b30dd504be616933605953984582f80cd5595d6
Valerockk <61026077+Valerockk@users.noreply.github.com>
2020-08-05 16:20:46 +0300

    Правки ДЗ №7

--- tasks-02.js
+++ tasks-02.js
@@ -10,7 +10,7 @@ const ingredients = [
 const ingredHtml = document.querySelector("#ingredients");
 const arr = [];
 
-const createLi = ingredients.forEach((el) => {
+const createLi = ingredients.map((el) => {
   const ing = document.createElement('li');
   arr.push(ing);
   ing.textContent = el;
